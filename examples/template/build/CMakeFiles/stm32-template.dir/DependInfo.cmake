bc306b5a3643e0f1d167ed1ee67887854be721a4
Chris Browning <cbrowning@yahoo.com>
2021-06-11 11:29:52 -0500

    initial commit

---
+++ examples/template/build/CMakeFiles/stm32-template.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/gcc/startup_stm32f031x6.s" "/home/cbrowning/src/stm32-bm/examples/template/build/CMakeFiles/stm32-template.dir/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/gcc/startup_stm32f031x6.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F0"
+  "STM32F031x6"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Include"
+  "/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/system_stm32f0xx.c" "/home/cbrowning/src/stm32-bm/examples/template/build/CMakeFiles/stm32-template.dir/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/system_stm32f0xx.c.obj"
+  "/home/cbrowning/src/stm32-bm/examples/template/main.c" "/home/cbrowning/src/stm32-bm/examples/template/build/CMakeFiles/stm32-template.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F0"
+  "STM32F031x6"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Include"
+  "/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ examples/template/build/CMakeFiles/stm32-template.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stm32-template.dir/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/gcc/startup_stm32f031x6.s.obj"
+  "CMakeFiles/stm32-template.dir/home/cbrowning/src/STM32CubeF0/Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/system_stm32f0xx.c.obj"
+  "CMakeFiles/stm32-template.dir/main.c.obj"
+  "stm32-template.elf"
+  "stm32-template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/stm32-template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
